f6fde89ff37b817f5fabc7db79b0575dcedb4a03
Cheleste1994 <wolverines@tut.by>
2024-01-25 15:38:08 +0300

    feat: completed the fs task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,13 @@
+import {readFile} from 'fs';
+
 const read = async () => {
-    // Write your code here 
+   readFile('./src/fs/files/fileToRead.txt', (error, data) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    }
+
+    console.log(data.toString());
+   })
 };
 
 await read();
