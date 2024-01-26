c8d628351a684216cbd9d39efcb6946ed2077dc7
Cheleste1994 <wolverines@tut.by>
2024-01-26 12:50:01 +0300

    feat: completed the hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from "crypto";
+import { readFile, createReadStream } from "fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = createReadStream(
+    "./src/hash/files/fileToCalculateHashFor.txt"
+  );
+
+  readStream.on("data", (chunk) => {
+    const hash = createHash("sha256");
+    const finalHex = hash.update(chunk.toString()).digest("hex");
+
+    console.log(`${finalHex}\n`);
+  });
 };
 
 await calculateHash();
