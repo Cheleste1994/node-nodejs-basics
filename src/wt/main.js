f7902a5d82985e26347745e8d7888acbebb7d60b
Cheleste1994 <wolverines@tut.by>
2024-01-28 17:02:29 +0300

    feat: completed the wt task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,36 @@
+import { Worker, isMainThread } from "worker_threads";
+import { cpus } from "os";
+
+const numCores = cpus().length;
+
+const resultWorkers = [];
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const worker = new Worker("./src/wt/worker.js", { workerData: i + 10 });
+
+    worker.on("message", (result) => {
+      resultWorkers.push(result);
+    });
+
+    worker.on("error", (result) => {
+      resultWorkers.push(result);
+    });
+
+    workers.push(worker);
+  }
+
+  Promise.all(
+    workers.map(
+      (worker) => new Promise((resolve) => worker.on("exit", resolve))
+    )
+  ).then(() => {
+    console.table(resultWorkers);
+  });
 };
 
-await performCalculations();
+if (isMainThread) {
+  await performCalculations();
+}
